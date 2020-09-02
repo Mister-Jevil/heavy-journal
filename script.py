aabff12b99559b122ff267b2d9b53c2e36a7f692
espriminati <espriminati@hotmail.com>
2020-09-03 01:02:00 +0300

    Add some print messages

--- script.py
+++ script.py
@@ -11,8 +11,8 @@ saveEveryXKills = 10 # Save the file every X kills. Setting this to 1 or lower w
 saveEveryXKills = max(1, saveEveryXKills)
 remainingKillsToSave = saveEveryXKills
 logpath = "/tf/console.log" # DO NOT CHANGE THIS
-killedText = "Just killed {victim} using my {weapon}. Feeling good! {crit}"
-critText = "critted them btw lol"
+
+# SQLite stuff
 db = sqlite3.connect("kills.db")
 sqlcommand = "INSERT INTO kills (victim,weapon,crit) VALUES (?,?,?);"
 
@@ -30,6 +30,7 @@ def watch(fn, words):
 			time.sleep(0.5)
 
 def saveBeforeExit():
+	print "[SAVE] Saving before exit."
 	db.commit()
 	db.close()
 
@@ -40,37 +41,40 @@ atexit.register(saveBeforeExit)
 # weapon: (string) The ID of the weapon you just killed with
 # crit:  (boolean) Was the last hit a critical hit?
 def killed(victim, weapon, crit):
-	if crit:
-		finalText = killedText.format(
-			victim=victim, weapon=weapon, crit=critText)
-	else:
-		finalText = killedText.format(victim=victim, weapon=weapon, crit="")
-	print(finalText)
+	global remainingKillsToSave
+	print "[KILL]",
+	if crit: 
+		print "[CRITICAL HIT]",
+	print victim + " using " + weapon
 
 	db.execute(sqlcommand, (victim, weapon, (1 if crit else 0)))
 
-	remainingKillsToSave -= 1
-	if (remainingKillsToSave == 0):
-		print("%s kills since last save, saving..." % saveEveryXKills)
+	remainingKillsToSave = remainingKillsToSave - 1 # I know that -=1 is athing but for some reason it doesnt work
+	if remainingKillsToSave == 0:
+		print "[SAVE] %s kills since last save, saving..." % saveEveryXKills
 		db.commit()
+		remainingKillsToSave = saveEveryXKills
 
 # Check if the installation directory is valid and exists
 if not (os.path.exists(installpath) and os.path.isdir(installpath)) or (sys.platform.startswith("win") and installpath.startswith("/")):
-	print("Installation directory is invalid. Please make sure the installpath variable is correct")
+	print "[ERROR] Installation directory is invalid. Please make sure the installpath variable is correct" 
 	sys.exit(1)
 
 # Check if we have an actual 'console.log' file
 if not os.path.exists(installpath + logpath):
-	print("console.log file doesnt exist. Please make sure that you started TF2 using '-condebug -conclearlog -console' arguments")
+	print "[ERROR] console.log file doesnt exist. Please make sure that you started TF2 using '-condebug -conclearlog -console' arguments"
 	sys.exit(1)
 
+print "[INFO] Started reading kills"
 for triggeredWord, log in watch(installpath + logpath, ['killed', '"name" =']):
 	if log.startswith('"name" ='): # do not check triggeredWord because if someone has '"name" =' in their name, we dont want to change the name again 
 		name = log.replace('"', "").replace('name = ', "").replace(" ( def. unnamed )", "").replace("\n", "")
-		print(name)
+		print "[INFO] Name successfully set to %s" % name
 	else:
-		replacedArray = log.replace(
-			name + " killed ", "").replace(".\n", "").split(" with ")
-		killed(" with ".join(replacedArray[0:len(replacedArray)-1]), replacedArray[-1].replace(
-			". (crit)\n", ""), log.endswith("(crit)\n"))
-
+		if name == "":
+			print "[WARN] Name is not set. Please run 'name' in game console. Kills will not be counted if you do not set it"
+		elif log.startswith(name):
+			replacedArray = log.replace(
+				name + " killed ", "").replace(".\n", "").split(" with ")
+			killed(" with ".join(replacedArray[0:len(replacedArray)-1]), replacedArray[-1].replace(
+				". (crit)\n", ""), log.endswith("(crit)\n"))
